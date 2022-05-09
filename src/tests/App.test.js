daedcf48ca1a995c54fde0b8a0bb91075629e96c
Raquel Santos <raquelsantossilva2405@outlook.com>
2022-05-09 15:42:10 -0300

    Adicionando requisito 1

--- src/tests/App.test.js
+++ src/tests/App.test.js
@@ -1,1 +1,20 @@
-test('', () => {});
+import React from 'react';
+import { screen } from '@testing-library/react';
+// import userEvent from '@testing-library/user-event';
+import App from '../App';
+import renderWithRouter from '../RenderWithRouter';
+
+describe('Teste do componente "<App.js"/>', () => {
+  it('Testa se o topo da aplicação contém um conjunto fixo de links.', async () => {
+    renderWithRouter(<App />);
+    const homeEl = screen.getByRole('link', { name: 'Home' });
+    const aboutEl = screen.getByRole('link', { name: 'About' });
+    const favoritePokémonsEl = screen.getByRole('link', { name: 'Favorite Pokémons' });
+    expect(homeEl).toBeDefined();
+    expect(homeEl).toHaveTextContent('Home');
+    expect(aboutEl).toBeDefined();
+    expect(aboutEl).toHaveTextContent('About');
+    expect(favoritePokémonsEl).toBeDefined();
+    expect(favoritePokémonsEl).toHaveTextContent('Favorite Pokémons');
+  });
+});
